6e099b6a0abc0dc56a284aff9328cabf1650c4d5
alireza <alirezahekmati80@gmail.com>
2022-03-08 03:23:47 -0800

    port

---
+++ script.js
@@ -0,0 +1,11 @@
+let isOpenend =false
+function close(){
+    console.log("yo")
+    if(isOpenend){
+        document.querySelector(".side-bar").style.animation =" identifier2 .5s  forwards"
+    }else{
+        document.querySelector(".side-bar").style.animation =" identifier1 .5s  forwards"
+    }
+    isOpenend = !isOpenend
+}
+// document.body.addEventListener("click",close)
